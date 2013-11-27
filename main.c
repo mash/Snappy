ab2808ad84b31334fd5a01b56609f1725db89e72
Masakazu OHTSUKA <o.masakazu@gmail.com>
2013-11-27 15:49:35 +0900

    ad works

--- main.c
+++ main.c
@@ -19,9 +19,38 @@
 void setup_pins () {
     // pin3 is analog input, connected to variable registor
     // GND ~ VCC
+    DDRB &=~  _BV( DDB4 );
+
+    ADMUX  &=~ _BV( REFS0 ); // 0: Vcc used as analog reference
+    ADMUX  |=  _BV( ADLAR ); // ADC Left Adjust Result
+    ADMUX  |=  _BV( MUX1 );  // MUX1..0 = 10b ADC2 (PB4)
+    ADMUX  &=~ _BV( MUX0 );
+
+    ADCSRA &=~ _BV( ADPS2 ); // ADPS2..0 ADC Prescaler Selection: 1/8
+    ADCSRA |=  _BV( ADPS1 );
+    ADCSRA |=  _BV( ADPS0 );
+    ADCSRA |=  _BV( ADEN );  // enable
+    ADCSRA |=  _BV( ADSC );  // start conversion
 
     // pin5 is digital out
     DDRB |= _BV( DDB0 );
+}
+
+uint8_t readADC() {
+    ADCSRA |= _BV( ADIF ); // clear ADC Interrupt flag
+    ADCSRA |= _BV( ADSC ); // start
+
+    // wait til ADIF is 1
+    while (! (ADCSRA & _BV( ADIF ))) ;
+
+    return ADCH;
+}
+
+void delay_ms(uint8_t ms) {
+    uint8_t i;
+    for (i=0; i<ms; i++) {
+        _delay_ms(1);
+    }
 }
 
 int main(void)
@@ -29,11 +58,13 @@ int main(void)
     setup_pins();
 
     for (;;) {
+        uint8_t pin3 = readADC();
+
         PORTB |=  _BV( PORTB0 );
-        _delay_ms(200);
+        delay_ms(pin3);
 
         PORTB &=~ _BV( PORTB0 );
-        _delay_ms(200);
+        delay_ms(pin3);
     }
     return 0;   /* never reached */
 }
